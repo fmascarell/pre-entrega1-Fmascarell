7a2ea524ece33b874a13e9ca6df52822f1728779
fmascarell <72888717+fmascarell@users.noreply.github.com>
2023-10-13 00:41:31 -0300

    Delete CartWidget.jsx

--- CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-
-const CartWidget = () => {
-    return (
-        <div>
-            <img className='cartWidget' src='/img/carrito-de-compras.png' />
-        </div>
-    )
-}
-export default CartWidget
