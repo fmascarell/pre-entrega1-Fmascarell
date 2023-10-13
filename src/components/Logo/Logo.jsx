dfd16ff26acd9c1f0477a72fbfbeaac4833ab94f
fmascarell <72888717+fmascarell@users.noreply.github.com>
2023-10-13 00:43:59 -0300

    Add files via upload

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,8 @@
-export const Logo = () => {
+const Logo = () => {
     return (
         <div>
             <img className="logo" src='/img/helado.png' />
         </div>
     )
 }
+export default Logo
